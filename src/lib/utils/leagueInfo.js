8b54ce4c13065ffc5030d2b2b18d55272ee3aa94
dopiedopeman <120254237+dopiedopeman@users.noreply.github.com>
2022-12-10 17:09:07 +0100

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -206,10 +206,10 @@ export const managers = [
        "roster": 9,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
        "name": "lions50",
        "tookOver": 2022, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-    //   "location": "Brooklyn", // (optional)
+       "location": "Mainz", // (optional)
     //   "bio": "Lorem ipsum...",
        "photo": "/managers/Manu.jpg", // square ratio recommended (no larger than 500x500)
-       "fantasyStart": 2022, // (optional) when did the manager start playing fantasy football
+    //   "fantasyStart": 2022, // (optional) when did the manager start playing fantasy football
     //   "favoriteTeam": "nyj", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
     //   "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
     //   "rival": {
