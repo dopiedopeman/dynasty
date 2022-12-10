bb9ab71d88c3dbbcb6b109cb1acb2bcd560db944
dopiedopeman <120254237+dopiedopeman@users.noreply.github.com>
2022-12-10 22:49:16 +0100

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -123,7 +123,7 @@ export const managers = [
        "photo": "/managers/Maggi.jpg", // square ratio recommended (no larger than 500x500)
        "fantasyStart": 2016, // (optional) when did the manager start playing fantasy football
        "favoriteTeam": "sea", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-       "mode": "12", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
+       "mode": "last", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
        "rival": {
          name: "Chris", // Can be anything (usually your rival's name)
          link: 1, // manager array number within this array, or null to link back to all managers page
