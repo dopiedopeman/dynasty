1e3edd3810f1ee6b0b3bd16bfd60acef49c2659b
dopiedopeman <120254237+dopiedopeman@users.noreply.github.com>
2022-12-13 21:06:35 +0100

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -31,9 +31,9 @@ export const managers = [
        "name": "DopieDopeMan",
        "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "Mainz", // (optional)
-       "bio": "Seit Anfang an dabei. Eines der Gründungsmitglieder und der Gott-Commissioner.",
+   //    "bio": "Seit Anfang an dabei. Eines der Gründungsmitglieder und der Gott-Commissioner.",
        "photo": "/managers/Marcus.jpg", // square ratio recommended (no larger than 500x500)
-       "fantasyStart": 2015, // (optional) when did the manager start playing fantasy football
+   //    "fantasyStart": 2015, // (optional) when did the manager start playing fantasy football
        "favoriteTeam": "dal", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
     //   "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
    //    "rival": {
