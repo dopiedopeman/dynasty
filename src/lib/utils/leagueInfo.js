f52fe9ee8688733ad2637490d8d52bc1fa69543a
dopiedopeman <120254237+dopiedopeman@users.noreply.github.com>
2022-12-13 21:07:16 +0100

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,7 +1,7 @@
 /*   STEP 1   */
 export const leagueID = "786635113353039872"; // your league ID
 export const leagueName = "Mainzer Dynasty League"; // your league name
-export const dues = null; // (optional) used in template constitution page
+export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
